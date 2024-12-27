177511cba350017d78e1386fcc9128f7d42a0e55
radoicic <onecodestar@outlook.com>
2024-12-28 03:51:50 +0900

    video call socket

--- backend/websocket.js
+++ backend/websocket.js
@@ -6,16 +6,15 @@ const { emitNotificationForChat } = require("./controllers/socketController");
 const server = http.createServer();
 global.io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://104.236.35.101"], // Allow these origins
+    origin: ["http://localhost:3000", "http://192.168.148.178:3000", "http://104.236.35.101"], // Allow these origins
+    // origin: ["*"], // Allow these origins
     methods: ["GET", "POST"], // Allow these HTTP methods
     allowedHeaders: ["my-custom-header"], // Optional: Specify allowed headers
-    credentials: true, // Optional: Allow credentials like cookies
+    credentials: true, // Optional: Allow credentials like cookies    
   },
 });
 
 io.on("connection", (socket) => {
-  // console.log(socket.id);
-
   socket.on("register", (user_id) => {
     console.log("register", user_id);
     socket.join(user_id);
@@ -36,9 +35,36 @@ io.on("connection", (socket) => {
     );
   });
 
+  socket.on("callUser", (data) => {
+    console.log('callUser------------', data.userToCall);
+    io.to(data.userToCall).emit("hey", {
+      signal: data.signalData,
+      from: data.from,
+    });
+  });
+
+  socket.on("acceptCall", (data) => {
+    console.log('acceptCall-----------');
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+
+  socket.on("close", (data) => {
+    console.log('close');
+    io.to(data.to).emit("close");
+  });
+
+  socket.on("rejected", (data) => {
+    io.to(data.to).emit("rejected");
+  });
+
+  socket.on("sendMessage", (data) => {
+    io.to(data.to).emit("receiveMessage", data.text);
+  });
+
   socket.on("disconnect", () => {
     // console.log("A user disconnected");
   });
+
 });
 
 io.listen(8080);
